9ebde4d272693d6f4f28c9f9d97003f39396e638
MMMamich <MMMamich@gmail.com>
2017-09-12 11:08:55 -0700

    updated models

--- models/posts.js
+++ models/posts.js
@@ -7,18 +7,10 @@ module.exports = function(sequelize, DataTypes){
         len: [1, 140]
       }
     },
-<<<<<<< HEAD
-=======
-
->>>>>>> 67c6d92ea2c0b5bc8456d16a3c86f5eae58b8dea
     longitude: {
       type: DataTypes.FLOAT(1, 20),
       allowNull: false
     },
-<<<<<<< HEAD
-=======
-
->>>>>>> 67c6d92ea2c0b5bc8456d16a3c86f5eae58b8dea
     latitude: {
       type: DataTypes.FLOAT(1, 20),
       allowNull: false
--- models/users.js
+++ models/users.js
@@ -1,33 +1,21 @@
 module.exports = function(sequelize, DataTypes){
   var Users = sequelize.define("Users", {
-<<<<<<< HEAD
-=======
     name: {
       type: DataTypes.STRING,
       allowNull: false
     },
-
->>>>>>> 67c6d92ea2c0b5bc8456d16a3c86f5eae58b8dea
     username: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-<<<<<<< HEAD
-        len[1, 12]
-=======
         len: [1, 12]
->>>>>>> 67c6d92ea2c0b5bc8456d16a3c86f5eae58b8dea
       }
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-<<<<<<< HEAD
-        len[1, 12]
-=======
         len: [1, 12]
->>>>>>> 67c6d92ea2c0b5bc8456d16a3c86f5eae58b8dea
       }
     }
   });

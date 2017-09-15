19f7df95fdc0815ec6f3563ae1fce86f24f491fb
Josiah De La Torre <jodelat@yahoo.com>
2017-09-15 16:29:11 -0700

    heroku test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 var db = require("./models");
 
